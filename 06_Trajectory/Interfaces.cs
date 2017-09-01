e7ff02591e0b99fc7e1c532172dff1cf2b2873ef
HawkWang <230574796@qq.com>
2017-09-01 16:58:31 +0800

    New Deviation
    RT

--- 06_Trajectory/Interfaces.cs
+++ 06_Trajectory/Interfaces.cs
@@ -2,41 +2,38 @@
 
 namespace GameUtil.Trajectory
 {
-
-
     /// <summary>
-    /// 适用于飞精瞄状态下的弹道，精瞄状态下有瞄具的真实位置决定弹道射线。
+    /// 适用于非精瞄状态下的弹道，精瞄状态下有瞄具的真实位置决定弹道射线。
+    /// 做一个简化模型，非真实弹道。
     /// </summary>
-    public class TrajectoryParams:ScriptableObject
+    public class SimpleDeviationParams:ScriptableObject
     {
-        [Tooltip("有效Layer，不填就没用，不填默认ConsiderTrigger无效")]
-        public int valid_layers;
+        [Tooltip("射线有效Layer")]
+        public string[] consider_layers;
         [Tooltip("是否考虑Trigger,默认False")]
         public bool consider_trigger = false;
 
-        [Tooltip("每米误差，如果精度不够，考虑改成，每十米误差。待定")]
-        public float min_deviation;
-        [Tooltip("最大误差，连续开枪也不会超过这个值")]
-        public float max_deviation;
-        [Tooltip("偏移放大速度")]
-        public float deviation_speed;
-        [Tooltip("缩圈速度，应该比放大速度略小")]
-        public float shrink_speed;
+        [Tooltip("静止时候的最高精度")]
+        public float best_deviation = 0.001f;
+        [Tooltip("最大移动误差，连续开枪也不会超过这个值")]
+        public float worst_deviation = 0.04f;
 
-        [Tooltip("以下值影响放大，最大系数，不影响缩圈系数，暂定缩圈系数是恒定的这是为了简化模型。")]
-        public float stand_fire_rate = 1.0f;
-        public float stand_move_rate = 1.0f;
-        public float couch_fire_rate = 0.75f;
-        public float couch_move_rate = 0.75f;
+        [Tooltip("一次开枪带来的瞬间Deviation增长值,注意这个值不会乘以DeltaTime")]
+        public float impluse_deviation_delta = 0.01f;
+        
+        public float deviation_acceration = 1.0f;
+        public float impluse_deviation_acceration = 10.0f;
+        public float return_from_impluse = 20.0f;
     }
 
     public interface iTrajectory
     {
-        TrajectoryParams trajectory_params { get; set; }        
-        void OnMove(bool is_begin);
-        void OnAir(bool is_begin);
+        SimpleDeviationParams deivation_params { set; }
+        //do not insert speed value， insert the current speed / standard max speed
+        float move_speed_rate { set; }
+        float deviation { get; }
         bool Fire(Vector3 start_pos, Vector3 direction, out RaycastHit hit);
-        void TickShrinkAim(float delta_time);
+        void TickDeviation(float delta_time);
     }
 }
 
--- 06_Trajectory/SphereDeviationTrajectory.cs
+++ 06_Trajectory/SphereDeviationTrajectory.cs
@@ -10,36 +10,63 @@ namespace GameUtil.Trajectory
     /// </summary>
     public class SphereDeviationTrajectory : MonoBehaviour,iTrajectory
     {
-        public TrajectoryParams trajectory_params
+        SimpleDeviationParams param;
+
+        const float C_MOVE_SPEED_THRESHOLD = 0.1f;
+        float current_deviation;
+        float current_deviation_accerlation;
+        float target_deviation;        
+        float speed_rate;
+
+        public SimpleDeviationParams deivation_params
         {
-            get
+            set 
             {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-                throw new System.NotImplementedException();
+                param = value; 
             }
         }
 
-        public void OnMove(bool is_begin)
+        public float move_speed_rate
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnAir(bool is_begin)
-        {
-            throw new System.NotImplementedException();
+            set 
+            {
+                speed_rate = value;
+            }
         }
 
         public bool Fire(Vector3 start_pos, Vector3 direction, out RaycastHit hit)
         {
-            throw new System.NotImplementedException();
+            target_deviation += param.impluse_deviation_delta;
+            ClampTargetDeviation();
+
+            current_deviation_accerlation = param.impluse_deviation_acceration;   
+         
+            return true;
         }
 
-        public void TickShrinkAim(float delta_time)
+
+        public void TickDeviation(float delta_time)
         {
-            throw new System.NotImplementedException();
+            ClampTargetDeviation();
+
+            current_deviation = Mathf.Lerp(current_deviation, target_deviation, current_deviation_accerlation);
+
+            current_deviation_accerlation = Mathf.Lerp(current_deviation_accerlation, param.deviation_acceration, param.return_from_impluse);
+        }
+
+        private void ClampTargetDeviation() 
+        {
+            float rate = 1.0f + speed_rate;
+            target_deviation = Mathf.Clamp(target_deviation, param.best_deviation * rate, param.worst_deviation * rate);
+        }
+
+
+        public float deviation
+        {
+            get 
+            {
+                return current_deviation;
+            }
         }
     }
 }
