73a35908d9c00314dff3f42343470575f47111bc
HawkWang <230574796@qq.com>
2017-09-05 21:40:45 +0800

    MonoEx
    MonoEx 对monobehavior做了一些便利扩展

---
+++ 01_Shared/MonoEx.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameUtil
+{
+
+	public class MonoEx : MonoBehaviour 
+	{
+		private Animator _animator;
+		protected Animator animator
+		{
+			get
+			{
+				if (_animator == null) 
+				{
+					_animator = GetComponent<Animator> ();
+				}
+				return _animator;
+			}			
+		}
+
+		private Transform _cachedTransform;
+		private Transform cachedTransform
+		{
+			get
+			{
+				if (_cachedTransform == null) 
+				{
+					_cachedTransform = transform;
+				}
+				return _cachedTransform;
+			}
+		}
+	}	
+}
+
