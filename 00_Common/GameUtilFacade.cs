b06e13e986393a865b1c01192f8296d3a9a643b6
HawkWang <230574796@qq.com>
2017-09-22 09:23:22 +0800

    Adjust Structure

---
+++ 00_Common/GameUtilFacade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameUtil
+{
+    public class GameUtilFacade : Singleton<GameUtilFacade>
+    {
+        public override void OnAwake()
+        {
+            //TODO:Init all that need to be init.        
+        }
+    }
+
+}
